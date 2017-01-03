593388825f1d9b593c7341dbd986ee30c9f56633
justboris <boriskan@gmail.com>
2017-01-03 12:46:22 -0500

    push to git

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mark2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mark2App
+ */
+angular.module('mark2App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
