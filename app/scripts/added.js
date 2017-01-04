457a2dec8a0ffa7aac0e5ae3927df2eebb65381a
justboris <boriskan@gmail.com>
2017-01-04 09:36:46 -0500

    last night's progress

--- app/scripts/added.js
+++ app/scripts/added.js
@@ -9,7 +9,7 @@ jQuery(document).ready(function() {
 		}
 		next.children(':first-child').clone().appendTo(jQuery(this));
 	  
-		for (var i=0;i<2;i++) {
+		for (var i=0;i<3;i++) {
 			next=next.next();
 			if (!next.length) {
 				next = jQuery(this).siblings(':first');
